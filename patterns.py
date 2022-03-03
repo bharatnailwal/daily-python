12dc44f71db95873bde28914a69ff8f47d030cc4
Bharat Nailwal <42619150+bharatnailwal@users.noreply.github.com>
2022-03-03 23:13:30 +0530

    Patterns coding question
    daily-code-program-1

---
+++ patterns.py
@@ -0,0 +1,103 @@
+#How to approch
+# 1 > run the outer for loop [rows]
+# 2 > Identofy, every row numbers, how many colums are there
+# 3 > types of elements in the colums, wghat do you need to print
+
+#x x x x x
+#x x x x x
+#x x x x x
+#x x x x x
+#x x x x x
+
+
+def patterns(num):
+    for i in range(num):
+        for j in range(num):
+            print ("x",end=' ')
+
+
+print(patterns(10))
+
+
+#x
+#x x
+#x x x
+#x x x x
+#x x x x x
+
+
+def patterns(num):
+    for i in range(num):
+        for j in range(i+1):
+            print ("x",end=' ')
+        print()
+
+print(patterns(10))
+
+
+
+
+#x x x x x
+#x x x x 
+#x x x
+#x 
+
+
+
+def patterns(num):
+    for i in range(num):
+        for j in range(i,num):
+            print ("x",end=' ')
+        print()
+        
+print(patterns(10))
+
+
+
+
+# x                     
+# x x                   
+# x x x                 
+# x x x x               
+# x x x x x             
+# x x x x x x           
+# x x x x x x x         
+# x x x x x x x x       
+# x x x x x x x x x     
+# x x x x x x x x x x  
+
+
+def patterns(num):
+    for i in range(num):
+        # for j in range(i,num):
+        #      print (" ",end=' ')
+        for j in range(i+1):
+            print ("x",end=' ')
+        for j in range(i,num):
+            print (" ",end=' ')
+        print()
+        
+print(patterns(10))
+
+
+
+#                     x 
+#                   x x 
+#                 x x x 
+#               x x x x 
+#             x x x x x 
+#           x x x x x x 
+#         x x x x x x x 
+#       x x x x x x x x 
+#     x x x x x x x x x 
+#   x x x x x x x x x x 
+
+def patterns(num):
+    for i in range(num):
+        for j in range(i,num):
+            print (" ",end=' ')
+        for j in range(i+1):
+            print ("x",end=' ')
+        print()
+        
+print(patterns(10))
