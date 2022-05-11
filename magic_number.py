82f12d06e37df2d263466e4f49e46c1745477ef3
Bharat Nailwal <42619150+bharatnailwal@users.noreply.github.com>
2022-05-11 22:10:26 +0530

    Create magic_number.py

---
+++ magic_number.py
@@ -0,0 +1,18 @@
+def ask_number(nb_min,nb_max):
+    number_str = input(f"What is the magic number (between {nb_min} and {nb_max}) ? ")
+    number_int = int(number_str)
+    return number_int
+
+
+min_number = 1
+max_number = 10
+magic_number = 5
+
+number = ask_number(min_number,max_number)
+
+if number == magic_number:
+    print("You won! ")
+elif number > magic_number:
+    print("the number is lower")
+elif number < magic_number:
+    print("the number is greater")
