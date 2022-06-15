2cc8af515feb17f20282ce439f5094f66c756731
Bharat Nailwal <42619150+bharatnailwal@users.noreply.github.com>
2022-06-15 18:18:27 +0530

    list_dir

---
+++ list_dir.py
@@ -0,0 +1,12 @@
+# Importing the os library
+import os
+ 
+# The path for listing items
+path = '.'
+ 
+# The list of items
+files = os.listdir(path)
+ 
+# Loop to print each filename separately
+for filename in files:
+    print(filename)
